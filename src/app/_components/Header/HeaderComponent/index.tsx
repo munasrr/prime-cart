f713debfa1cd5df02eeea7d3f9692d03b604454a
Anwar Abdi <Anwarabdi2090@gmail.com>
2024-11-13 11:25:49 +0300

    feat: add Header component with async data fetching from payload API

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from './../../../../payload/payload-types';
+
+const HeaderComponent = ({header} :{header: Header}) => {
+  return (
+  <nav>
+    
+  </nav>
+  )
+}
+
+export default HeaderComponent
