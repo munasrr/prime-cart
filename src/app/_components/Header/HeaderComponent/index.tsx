7c1875a38fa7e41a65b065d41503b25276dd1323
Anwar Abdi <Anwarabdi2090@gmail.com>
2024-11-13 13:48:51 +0300

    - Removed  component for streamlined navigation structure

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -11,7 +11,7 @@ import { Gutter } from '../../Gutter'
 import { HeaderNav } from '../Nav'
 
 import classes from './index.module.scss'
-import MobileNav from '../MobileNav'
+
 
 const HeaderComponent = ({ header }: { header: Header }) => {
   const pathname = usePathname()
@@ -28,7 +28,6 @@ const HeaderComponent = ({ header }: { header: Header }) => {
         </Link>
 
         <HeaderNav header={header} />
-        <MobileNav header={header} />
       </Gutter>
     </nav>
   )
